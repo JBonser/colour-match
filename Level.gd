0e8581d114a031e99cad276d8273dfa69ff31b43
Jordan Bonser <jordan_bonser@live.co.uk>
2018-08-23 22:26:45 +0100

    Removed Use of CanvasLayer and Added Containers
    I was using CanvasLayer Items unnecessarily which was
    breaking my main level when I switched to use containers.
    Also moved the start button out of the HUD as that doesn't
    make any sense.

--- Level.gd
+++ Level.gd
@@ -7,14 +7,14 @@ var colour_change_speed = 0.05
 
 func _ready():
     $LevelBackground.color = LevelColour
-    $LevelCanvas/LevelColourRect.color = LevelColour
+    $LevelColourRect.color = LevelColour
     set_process(false)
     
 
 func _process(delta):
     if Input.is_action_just_pressed("game_main_input"):
         colour_change = true
-        $LevelCanvas/MessageLabel.text = ""
+        $MessageLabel.text = ""
     elif colour_change and Input.is_action_just_released("game_main_input"):
         colour_change = false
         level_evaluate()
@@ -26,22 +26,21 @@ func _process(delta):
             $LevelBackground.color.h - 1.0
 
 func level_evaluate():
-    $LevelCanvas/MessageLabel.text = "Level Finished"
-    $LevelCanvas/UserChosenColourRect.color = $LevelBackground.color
+    $MessageLabel.text = "Level Finished"
+    $UserChosenColourRect.color = $LevelBackground.color
     $LevelBackground.color = Color(0.5,0.5,0.5,1.0)
-    $LevelCanvas/UserChosenColourRect.show()
-    $LevelCanvas/LevelColourRect.show()
+    $UserChosenColourRect.show()
+    $LevelColourRect.show()
     
-    
-    
+
 func start_level():
     $LevelBackground.show()
-    $LevelCanvas/MessageLabel.show()
+    $MessageLabel.show()
     $ColourShowTimer.start()
 
 
 func _on_ColourShowTimer_timeout():
-    $LevelCanvas/MessageLabel.text = "Finger on\nscreen to\nstart!"
+    $MessageLabel.text = "Finger on\nscreen to\nstart!"
     $LevelBackground.color.s = LevelColour.s
     $LevelBackground.color.v = LevelColour.v
     $LevelBackground.color.h = 0.7
--- Main.gd
+++ Main.gd
@@ -1,13 +1,6 @@
 extends Node
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
-#func _process(delta):
-#    # Called every frame. Delta is time since last frame.
-#    # Update game logic here.
-#    pass
-
-func _on_HUD_start_game():
+func _on_StartButton_pressed():
+    $VBoxContainer/Title.hide()
+    $VBoxContainer/StartButton.hide()
     $Level.start_level()
