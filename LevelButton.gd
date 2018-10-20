8d7abec791b8b83e5a0b93d8327210b958777f66
Jordan Bonser <jordan_bonser@live.co.uk>
2018-10-20 12:35:03 +0100

    Added Achieved Stars to Score LevelScreen
    This involved a lot of messing about with
    Gimp and also TextureButton.

--- LevelButton.gd
+++ LevelButton.gd
@@ -1,24 +1,37 @@
-extends Button
+extends TextureButton
+
+var star_achieved = preload("res://artwork/star_achieved_small.png")
 
 export(Color) var LevelColour
 export var LevelStar1Score = 0
 export var LevelStar2Score = 0
 export var LevelStar3Score = 0
+export var Number = "0"
 
 signal start_level
 
 func _ready():
-    var star_requirement = int(globals.Levels[text]["star_requirement"])
+    $MarginContainer/VBoxContainer/LevelNumber.text = Number
+    var star_requirement = int(globals.Levels[Number]["star_requirement"])
     if star_requirement > globals.calculate_total_stars():
         self.disabled = true
-    if globals.SaveData["completed_levels"].has(text):
-        set("custom_colors/font_color", Color(0,0,0))
+    if globals.SaveData["completed_levels"].has(Number):
+        calculate_stars(globals.SaveData["completed_levels"][Number]["stars"])
         
 
 func _on_LevelButton_button_up():
-    globals.LevelColour = Color(globals.Levels[text]["colour"])
-    globals.LevelNumber = text
+    globals.LevelColour = Color(globals.Levels[Number]["colour"])
+    globals.LevelNumber = Number
     globals.LevelStar1Score = LevelStar1Score
     globals.LevelStar2Score = LevelStar2Score
     globals.LevelStar3Score = LevelStar3Score
     get_tree().change_scene("res://Level.tscn")
+
+
+func calculate_stars(stars):
+    if stars >= 1:
+        $MarginContainer/VBoxContainer/Stars/Star1.texture = star_achieved
+    if stars >= 2:
+        $MarginContainer/VBoxContainer/Stars/Star2.texture = star_achieved
+    if stars >= 3:
+        $MarginContainer/VBoxContainer/Stars/Star3.texture = star_achieved
--- LevelButtonRow.gd
+++ LevelButtonRow.gd
@@ -1,17 +1,17 @@
 extends HBoxContainer
 
-export var num_levels = 4
+export var num_levels = 3
 export var starting_level_number = 1
 
 var level_button = preload("res://LevelButton.tscn")
 
+
 func _ready():
     for i in range(0, num_levels):
         var btn_instance = level_button.instance()
-        btn_instance.text = str(i+starting_level_number)
+        btn_instance.Number = str(i+starting_level_number)
         btn_instance.LevelColour = Color("53c0ae")
         btn_instance.LevelStar1Score = 200
         btn_instance.LevelStar2Score = 250
         btn_instance.LevelStar3Score = 300
-        
         add_child(btn_instance)
